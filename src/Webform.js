b55100c7469957072ccc8a9acafc38f429e2827a
Bülent Vural <bulent.vural@zerodensity.tv>
2022-03-29 17:55:08 +0300

    Fix: Grid rows disappear, bump version to 8.2.0
    Signed-off-by: Bülent Vural <bulent.vural@zerodensity.tv>

--- src/Webform.js
+++ src/Webform.js
@@ -872,30 +872,8 @@ export default class Webform extends NestedDataComponent {
   }
 
   manuallySetValue(submission, flags = {}) {
-    if (!submission || !submission.data) {
-      submission = { data: {} };
-    }
-
-    // Metadata needs to be available before setValue
-    this._submission.metadata = submission.metadata || {};
-    this.editing = !!submission._id;
-
-    // Set the timezone in the options if available.
-    if (
-      !this.options.submissionTimezone &&
-      submission.metadata &&
-      submission.metadata.timezone
-    ) {
-      this.options.submissionTimezone = submission.metadata.timezone;
-    }
-
-    const changed = super.manuallySetValue(submission.data, flags);
-    if (!flags.sanitize) {
-      this.mergeData(this.data, submission.data);
-    }
-
-    submission.data = this.data;
-    this._submission = submission;
+    const changed = this.setValue(submission, flags);
+    this.lastManuallySetValue = submission;
     return changed;
   }
 
